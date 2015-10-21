17675ebbb5f90f261db5940eb1a3c5180499c64c
eriksf <erik@jcvi.org>
2015-10-21 13:58:33 -0400

    added older dependency now hitting adama

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,54 +5,45 @@
 
   var appContext = $('[data-app-name="erik-old-test-app"]');
 
-  /* Generate Agave API docs */
+    // jshint unused: false
+  // jshint camelcase: false
+  // Wait for Agave to Bootstrap before executing our code.
   window.addEventListener('Agave::ready', function() {
-    var Agave, help, helpItem, helpDetail, methods, methodDetail;
-
-    Agave = window.Agave;
-
-    appContext.html('<h2>Hello AIP Science App &plus; Agave API!</h2><div class="api-help list-group"></div><hr><div class="api-info"></div><br>');
-
-    help = $('.api-help', appContext);
-
-    $.each(Agave.api.apisArray, function(i, api) {
-      helpItem = $('<a class="list-group-item">');
-      help.append(helpItem);
-
-      helpItem.append($('<h4>').text(api.name).append('<i class="pull-right fa fa-toggle-up"></i>'));
-      helpDetail = $('<div class="api-help-detail">');
-      helpDetail.append($('<p>').text(api.description));
-      helpDetail.append('<h5>Methods</h5>');
-      methods = $('<ul>');
-      $.each(api.help(), function(i, m) {
-        methodDetail = $('<li>');
-        methodDetail.append('<strong>' + m + '</strong>');
-        var details = api[m.trim()].help();
-        if (details) {
-          methodDetail.append('<br>').append('Parameters');
-          methodDetail.append('<p style="white-space:pre-line;">' + details + '</p>');
-        }
-        methods.append(methodDetail);
-      });
-      helpDetail.append(methods);
-      helpItem.append(helpDetail.hide());
-    });
-
-    $('.api-help > a', appContext).on('click', function() {
-      if (! $(this).hasClass('list-group-item-info')) {
-        // close other
-        $('.api-help > a.list-group-item-info', appContext).removeClass('list-group-item-info').find('.fa').toggleClass('fa-toggle-up fa-toggle-down').end().find('.api-help-detail').slideToggle();
-      }
-
-      $(this).toggleClass('list-group-item-info');
-      $('.fa', this).toggleClass('fa-toggle-up fa-toggle-down');
-      $('.api-help-detail', this).slideToggle();
-    });
-
-    var info = $('.api-info', appContext);
-    info.addClass('text-center');
-    info.append('<p>' + Agave.api.info.title + ': ' + Agave.api.info.description + '</p>');
-    info.append('<p><a href="mailto:' + Agave.api.info.contact + '">Contact</a> | <a href="' + Agave.api.info.license + '">License</a> | <a href="' + Agave.api.info.license + '">Terms of use</a></p>');
+    var Agave = window.Agave;  
+    var successFunction = function(response) { 
+    
+      if(response.obj.status !== 'success') { 
+        console.log('There was a problem: ' + 
+          response.obj.message);
+      } else { 
+        var profile = response.obj.result;
+        console.log(JSON.stringify(profile, null, 2));
+        $('.profile-name', appContext).text(profile.username);
+        
+          var vcard = $('.vcard', appContext);
+          vcard.find('.fn').text(profile.full_name);
+          vcard.find('.email').text(profile.email);
+          vcard.find('.tel-primary').text(profile.phone || 'not specified');
+          vcard.find('.tel-secondary').text(profile.mobile_phone || 'not specified');
+          // do some date parsing
+          var parsedDate = profile.create_time.replace(
+            /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/,
+            '$1-$2-$3T$4:$5:$6'
+          );
+          vcard.find('.note').text(new Date(parsedDate).toLocaleString());
+          vcard.removeClass('hide');        
+        
+      } 
+    };
+    var failFunction = function(err) {
+      //do failure stuff!
+      console.log('Failure! ', err);
+    };
+    Agave.api.profiles.me(
+      null, //no input needed here
+      successFunction,
+      failFunction
+    );
   });
 
 })(window, jQuery);
